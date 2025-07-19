ebefd78288e9b1b6e4c3aa89951e8249119dc7a3
nng777 <nikodemusng@gmail.com>
2025-07-19 11:11:32 +0700

    Commit

--- task.py
+++ task.py
@@ -2,7 +2,7 @@
 Part 3: Practical Application
 Task 3.1: Document Classification Preparation
 Create a mini document classification system:
-1.Collect 20 documents from 2 different categories (e.g., sports vs technology news (from detik.com/cnnindonesia.com/kompas.com), positive vs negative reviews)
+1.Collect 20 documents from 2 different categories (e.g., sports vs technology news, positive vs negative reviews)
 2.Convert each document to both TF-IDF and Word2Vec representations
 3.Calculate similarity scores between documents using cosine similarity
 4.Analyze which method (TF-IDF vs Word2Vec) better separates the two categories
